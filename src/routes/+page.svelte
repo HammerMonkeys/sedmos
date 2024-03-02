2a85869ec3ec7ffeaf2d0e41754687748d2ed77d
masoniis <bott.m@comcast.net>
2024-03-02 12:14:30 -0700

    fixed up flex setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	const functions = ["x = y", "y = x", "x = y + 1", "y = x + 1"];
+	const functions = [
+		"x = y",
+		"y = x",
+		"x = y + 1",
+		"y = x + 1",
+		"2x^2 = 3",
+		"2y^2 = 3",
+		"x^2 + y^2 = 1",
+	];
 </script>
 
-<body class="flex-row columns-2">
-	<div id="functions" class="">
-		<div id="functioncol" class="flex-col">
-			{#each functions as func, index (index)}
-				<div>{func}</div>
-			{/each}
-		</div>
+<body class="flex flex-row columns-2 h-screen">
+	<div
+		id="functioncol"
+		class="flex flex-col flex-nowrap columns-1 h-max bg-red-50"
+	>
+		{#each functions as func, index (index)}
+			<div id="functioncontainer" class="m-2 p-2 bg-gray-50">{func}</div>
+		{/each}
 	</div>
-	<div>
-		<canvas id="canvas"> </canvas>
+	<div id="graph" class="w-max bg-blue-400">
+		<canvas id="canvas" class="w-max"> </canvas>
 	</div>
 </body>
 

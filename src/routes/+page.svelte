e6031b1b2bfe23b45e646b2adcb9c5a5aad15c87
Simon Walker <simon@simonwalker.tech>
2024-03-03 11:31:28 -0700

    merged, finally, well.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,13 +10,9 @@
 
 	let latex_funcs: string[] = [""];
 
-	$: {
-		let top_left = tlbl[0];
-		let bottom_right = tlbl ? tlbl[1] : null;
-		console.log(top_left);
-		console.log(bottom_right);
-	}
-	let tlbl = [];
+  let tlbl: [number, number][] = [];
+  $: top_left = tlbl ? tlbl[0] : null;
+  $: bottom_right = tlbl ? tlbl[1] : null;
 
 	function dragStart(event: DragEvent, index: number) {
 		event.dataTransfer!.setData("text/plain", `${index}`);
@@ -53,15 +49,35 @@
 			? buildRenderer(canvas, ctx, w2sConv, latex_funcs)
 			: null;
 
-	function renderAllChunks() {
-		if (!renderChunk) {
-			return;
-		}
+  function renderAllChunks() {
+    if (!renderChunk) {
+      return;
+    }
 
-		renderChunk([0, 0]);
-		renderChunk([1, 0]);
-		renderChunk([1, -1]);
-	}
+    if (!bottom_right || !top_left) {
+      return;
+    }
+
+    try {
+      const chunkWidth = 5;
+      for (
+        let x = Math.floor(top_left[0] / chunkWidth);
+        x < bottom_right[0] / chunkWidth + 0.5;
+        x++
+      ) {
+        for (
+          let y = Math.floor(bottom_right[1] / chunkWidth);
+          y < top_left[1] / chunkWidth + 0.5;
+          y++
+        ) {
+          // todo /20 should  correspond to chunkWidth
+          renderChunk([x, y]);
+        }
+      }
+    } catch {
+      // pass
+    }
+  }
 
 	onMount(() => {
 		ctx = canvas.getContext("2d")!;
@@ -203,107 +219,107 @@
 </script>
 
 <body class="flex columns-2 h-screen w-screen bg-bg-900">
-	<div
-		id="functioncol"
-		class="overflow-y-auto overflow-x-clip max-h-screen resize-x min-w-20 w-72"
-	>
-		{#each latex_funcs as _, index (index)}
-			<div
-				id="functioncontainer{index}"
-				class="flex flex-nowrap flex-1 text-primary-300 bg-secondary-500 focus-within:bg-accent-500 focus-within:text-primary-900"
-				draggable="true"
-				on:dragstart={(event) => dragStart(event, index)}
-				on:drop={(event) => drop(event, index)}
-				on:dragover={(event) => onDragOver(event)}
-				transition:slide={{ duration: 100, easing: cubicOut }}
-				animate:flip={{ duration: 300, easing: cubicOut }}
-				role="group"
-			>
-				<div class="w-12 text-xs pl-1 font-bold cursor-move">
-					{index + 1}
-				</div>
-				<MathQuill
-					class="bg-bg-900 text-primary-100 w-full p-4 min-h-16 border-b-[1] focus:border-accent-500 active:border-accent-500 border-secondary-500 flex"
-					bind:latex={latex_funcs[index]}
-					autofocus
-				/>
-			</div>
-		{/each}
-		<div
-			role="button"
-			id="functioncontainer"
-			class="flex flex-nowrap flex-1 text-primary-300 bg-secondary-500 bg-gradient-to-b from-secondary-500 from-1% to-bg-900 to-70%"
-			on:click={() => {
-				latex_funcs = [...latex_funcs, ""];
-			}}
-			on:keypress={(e) => {
-				if (e.key === "Enter") {
-					latex_funcs = [...latex_funcs, ""];
-				}
-			}}
-			tabindex="0"
-		>
-			<div class="w-12 text-xs pl-1 font-bold">
-				{latex_funcs.length + 1}
-			</div>
-			<MathQuill
-				class="bg-bg-900 text-primary-100 w-full p-4 min-h-16 border-t-[2] border-l-[0] border-b-[0] border-r-[0] flex cursor-pointer"
-			/>
-		</div>
-	</div>
-	<div id="graph" class="w-max bg-blue-100 flex-grow-[1]">
-		<canvas
-			bind:this={canvas}
-			id="canvas"
-			class="w-full h-full overflow-clip bg-bg-200"
-			tabindex="0"
-			on:keypress={(e) => {
-				if (e.key === "Z") {
-					scale = scale + 1;
-					latex_funcs = latex_funcs;
-				} else if (e.key === "z") {
-					scale = scale - 1;
-					latex_funcs = latex_funcs;
-				}
-			}}
-		>
-		</canvas>
-	</div>
+  <div
+    id="functioncol"
+    class="overflow-y-auto overflow-x-clip max-h-screen resize-x min-w-20 w-72"
+  >
+    {#each latex_funcs as _, index (index)}
+      <div
+        id="functioncontainer{index}"
+        class="flex flex-nowrap flex-1 text-primary-300 bg-secondary-500 focus-within:bg-accent-500 focus-within:text-primary-900"
+        draggable="true"
+        on:dragstart={(event) => dragStart(event, index)}
+        on:drop={(event) => drop(event, index)}
+        on:dragover={(event) => onDragOver(event)}
+        transition:slide={{ duration: 100, easing: cubicOut }}
+        animate:flip={{ duration: 300, easing: cubicOut }}
+        role="group"
+      >
+        <div class="w-12 text-xs pl-1 font-bold cursor-move">
+          {index + 1}
+        </div>
+        <MathQuill
+          class="bg-bg-900 text-primary-100 w-full p-4 min-h-16 border-b-[1] focus:border-accent-500 active:border-accent-500 border-secondary-500 flex"
+          bind:latex={latex_funcs[index]}
+          autofocus
+        />
+      </div>
+    {/each}
+    <div
+      role="button"
+      id="functioncontainer"
+      class="flex flex-nowrap flex-1 text-primary-300 bg-secondary-500 bg-gradient-to-b from-secondary-500 from-1% to-bg-900 to-70%"
+      on:click={() => {
+        latex_funcs = [...latex_funcs, ""];
+      }}
+      on:keypress={(e) => {
+        if (e.key === "Enter") {
+          latex_funcs = [...latex_funcs, ""];
+        }
+      }}
+      tabindex="0"
+    >
+      <div class="w-12 text-xs pl-1 font-bold">
+        {latex_funcs.length + 1}
+      </div>
+      <MathQuill
+        class="bg-bg-900 text-primary-100 w-full p-4 min-h-16 border-t-[2] border-l-[0] border-b-[0] border-r-[0] flex cursor-pointer"
+      />
+    </div>
+  </div>
+  <div id="graph" class="w-max bg-blue-100 flex-grow-[1]">
+    <canvas
+      bind:this={canvas}
+      id="canvas"
+      class="w-full h-full overflow-clip bg-bg-200"
+      tabindex="0"
+      on:keypress={(e) => {
+        if (e.key === "Z") {
+          scale = scale + 1;
+          latex_funcs = latex_funcs;
+        } else if (e.key === "z") {
+          scale = scale - 1;
+          latex_funcs = latex_funcs;
+        }
+      }}
+    >
+    </canvas>
+  </div>
 </body>
 
 <style lang="postcss">
-	:global(.mq-focused) {
-		box-shadow: none !important;
-		border-color: theme(colors.accent.500) !important;
-	}
+  :global(.mq-focused) {
+    box-shadow: none !important;
+    border-color: theme(colors.accent.500) !important;
+  }
 
-	:global(.mq-root-block) {
-		display: block !important;
-		align-self: center !important;
-		caret-color: white !important;
-	}
+  :global(.mq-root-block) {
+    display: block !important;
+    align-self: center !important;
+    caret-color: white !important;
+  }
 
-	:global(.mq-math-mode .mq-empty) {
-		caret-color: white !important;
-	}
-	:global(.mq-overline) {
-		caret-color: white !important;
-	}
-	:global(.mq-underline) {
-		caret-color: white !important;
-	}
-	:global(.mq-overarrow .mq-editable-field .mq-selection.mq-blur .mq-matrixed) {
-		caret-color: white !important;
-		background-color: white !important;
-		border-color: white !important;
-		color: white !important;
-	}
+  :global(.mq-math-mode .mq-empty) {
+    caret-color: white !important;
+  }
+  :global(.mq-overline) {
+    caret-color: white !important;
+  }
+  :global(.mq-underline) {
+    caret-color: white !important;
+  }
+  :global(.mq-overarrow .mq-editable-field .mq-selection.mq-blur .mq-matrixed) {
+    caret-color: white !important;
+    background-color: white !important;
+    border-color: white !important;
+    color: white !important;
+  }
 
-	:global(.mathquill-editable) {
-		color: white !important;
-		border-color: white !important;
-	}
-	:global(.mathquill-rendered-math .mq-cursor) {
-		background: white !important;
-	}
+  :global(.mathquill-editable) {
+    color: white !important;
+    border-color: white !important;
+  }
+  :global(.mathquill-rendered-math .mq-cursor) {
+    background: white !important;
+  }
 </style>

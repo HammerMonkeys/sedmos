94bb755bd6cc9324f2f4a2072bc738485bb101d3
masoniis <bott.m@comcast.net>
2024-03-05 11:55:54 -0700

    graph function class updates

---
+++ src/lib/graphfunction.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+export class GraphFunction {
+	latex: string;
+	color: string;
+	isVisible: boolean;
+
+	readonly key: number;
+
+	constructor(graphFunction?: Partial<GraphFunction>) {
+		this.latex = graphFunction?.latex ?? ""; // Latex expression
+		this.color = graphFunction?.color ?? ""; // Hex color code
+		this.isVisible = graphFunction?.isVisible ?? true; // Visibility boolean
+
+		if (graphFunction?.key) {
+			throw new Error("Keys are computed internally and should not be passed into the SidebarElement constructor");
+		}
+
+		this.key = uuidv4(); // Unique generated ID
+	}
+}

a2abdc35e921abc551261b408ffe811459f3030e
Simon Walker <simon@simonwalker.tech>
2024-03-06 20:15:14 -0700

    Revert "slightly revised graphfunc & added todo"
    This reverts commit 4c621445104888d90277634abe99066aa413537a.

--- src/lib/components/Graph.svelte
+++ src/lib/components/Graph.svelte
@@ -1,250 +1,249 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { ceil } from "mathjs";
-  import buildRenderer from "$lib/foreground";
-  import { GraphFunction } from "$lib/graphfunction";
-  // import log from "$lib/log";
+	import { onMount } from "svelte";
+	import { ceil } from "mathjs";
+	import buildRenderer from "$lib/foreground";
+	import { GraphFunction } from "$lib/graphfunction";
+	// import log from "$lib/log";
 
-  // let latex_funcs: string[] = [""];
-  export let graph_funcs: GraphFunction[] = [new GraphFunction()];
-  let latex_funcs: string[] = graph_funcs.map((f) => f.latex);
+	// let latex_funcs: string[] = [""];
+	export let graph_funcs: GraphFunction[] = [new GraphFunction()];
+	let latex_funcs: string[] = graph_funcs.map((f) => f.latex);
 
-  // TODO: update numerical api
-  $: {
-    latex_funcs = graph_funcs.map((f) => f.latex);
-  }
+	$: {
+		latex_funcs = graph_funcs.map((f) => f.latex);
+	}
 
-  let tlbl: [number, number][] = [];
-  $: top_left = tlbl ? tlbl[0] : null;
-  $: bottom_right = tlbl ? tlbl[1] : null;
+	let tlbl: [number, number][] = [];
+	$: top_left = tlbl ? tlbl[0] : null;
+	$: bottom_right = tlbl ? tlbl[1] : null;
 
-  //TODO: Rethink the drag algorithm eniterly for the grid, as well as the gridline generation
-  let canvas: HTMLCanvasElement;
-  let vporigin = { x: 0, y: 0 };
-  let dragging = false;
-  let previousPoint: { x: number; y: number } = { x: 0, y: 0 };
-  let scale = 20; // # of square units horizontally (higher = zoomed out)
-  let ctx: CanvasRenderingContext2D;
-  $: squareSize = canvas ? canvas.width / scale : 0;
+	//TODO: Rethink the drag algorithm eniterly for the grid, as well as the gridline generation
+	let canvas: HTMLCanvasElement;
+	let vporigin = { x: 0, y: 0 };
+	let dragging = false;
+	let previousPoint: { x: number; y: number } = { x: 0, y: 0 };
+	let scale = 20; // # of square units horizontally (higher = zoomed out)
+	let ctx: CanvasRenderingContext2D;
+	$: squareSize = canvas ? canvas.width / scale : 0;
 
-  // used in the foreground renderer
-  $: w2sConv = (x: number, y: number) => {
-    const canvasCoord = cartToCanvas({ x, y });
-    return [canvasCoord.x, canvasCoord.y];
-  };
+	// used in the foreground renderer
+	$: w2sConv = (x: number, y: number) => {
+		const canvasCoord = cartToCanvas({ x, y });
+		return [canvasCoord.x, canvasCoord.y];
+	};
 
-  $: renderChunk =
-    canvas != undefined && ctx != undefined
-      ? buildRenderer(canvas, ctx, w2sConv, latex_funcs)
-      : null;
+	$: renderChunk =
+		canvas != undefined && ctx != undefined
+			? buildRenderer(canvas, ctx, w2sConv, latex_funcs)
+			: null;
 
-  function renderAllChunks() {
-    if (!renderChunk) {
-      return;
-    }
+	function renderAllChunks() {
+		if (!renderChunk) {
+			return;
+		}
 
-    if (!bottom_right || !top_left) {
-      return;
-    }
+		if (!bottom_right || !top_left) {
+			return;
+		}
 
-    try {
-      for (let x = -5; x < 5; x++) {
-        for (let y = -3; y < 3; y++) {
-          renderChunk([x, y]);
-        }
-      }
+		try {
+			for (let x = -5; x < 5; x++) {
+				for (let y = -3; y < 3; y++) {
+					renderChunk([x, y]);
+				}
+			}
 
-      // const chunkWidth = 5;
-      // for (
-      //   let x = Math.floor(top_left[0] / chunkWidth);
-      //   x < bottom_right[0] / chunkWidth + 0.5;
-      //   x++
-      // ) {
-      //   for (
-      //     let y = Math.floor(bottom_right[1] / chunkWidth);
-      //     y < top_left[1] / chunkWidth + 0.5;
-      //     y++
-      //   ) {
-      //     // todo /20 should  correspond to chunkWidth
-      //     renderChunk([x, y]);
-      //   }
-    } catch (err) {
-      // pass
-    }
-  }
+			// const chunkWidth = 5;
+			// for (
+			//   let x = Math.floor(top_left[0] / chunkWidth);
+			//   x < bottom_right[0] / chunkWidth + 0.5;
+			//   x++
+			// ) {
+			//   for (
+			//     let y = Math.floor(bottom_right[1] / chunkWidth);
+			//     y < top_left[1] / chunkWidth + 0.5;
+			//     y++
+			//   ) {
+			//     // todo /20 should  correspond to chunkWidth
+			//     renderChunk([x, y]);
+			//   }
+		} catch (err) {
+			// pass
+		}
+	}
 
-  onMount(() => {
-    ctx = canvas.getContext("2d")!;
-    drawGrid();
+	onMount(() => {
+		ctx = canvas.getContext("2d")!;
+		drawGrid();
 
-    function resizeCanvas(): void {
-      canvas.width = canvas.clientWidth;
-      canvas.height = canvas.clientHeight;
-      drawGrid();
-    }
+		function resizeCanvas(): void {
+			canvas.width = canvas.clientWidth;
+			canvas.height = canvas.clientHeight;
+			drawGrid();
+		}
 
-    new ResizeObserver(resizeCanvas).observe(canvas);
+		new ResizeObserver(resizeCanvas).observe(canvas);
 
-    window.onresize = resizeCanvas;
-    resizeCanvas();
+		window.onresize = resizeCanvas;
+		resizeCanvas();
 
-    canvas.addEventListener("mousedown", (event) => {
-      dragging = true;
-      previousPoint = { x: event.clientX, y: event.clientY };
-    });
+		canvas.addEventListener("mousedown", (event) => {
+			dragging = true;
+			previousPoint = { x: event.clientX, y: event.clientY };
+		});
 
-    canvas.addEventListener("mousemove", (event) => {
-      if (dragging) {
-        vporigin.x += previousPoint.x - event.clientX;
-        vporigin.y += event.clientY - previousPoint.y;
-        previousPoint = { x: event.clientX, y: event.clientY };
-        drawGrid();
-      }
-    });
+		canvas.addEventListener("mousemove", (event) => {
+			if (dragging) {
+				vporigin.x += previousPoint.x - event.clientX;
+				vporigin.y += event.clientY - previousPoint.y;
+				previousPoint = { x: event.clientX, y: event.clientY };
+				drawGrid();
+			}
+		});
 
-    canvas.addEventListener("mouseup", (_) => {
-      dragging = false;
-    });
-  });
+		canvas.addEventListener("mouseup", (_) => {
+			dragging = false;
+		});
+	});
 
-  function cartToCanvas(coord: { x: number; y: number }) {
-    var squaresize = canvas.width / scale;
-    const xcenter = Math.round(scale / 2) * squaresize;
-    const ycenter = canvas.height / 2;
+	function cartToCanvas(coord: { x: number; y: number }) {
+		var squaresize = canvas.width / scale;
+		const xcenter = Math.round(scale / 2) * squaresize;
+		const ycenter = canvas.height / 2;
 
-    return {
-      x: coord.x * squaresize - vporigin.x + xcenter,
-      y: -coord.y * squaresize + vporigin.y + ycenter,
-    };
-  }
+		return {
+			x: coord.x * squaresize - vporigin.x + xcenter,
+			y: -coord.y * squaresize + vporigin.y + ycenter,
+		};
+	}
 
-  //TODO: Fix the y value conversion, seems to be slightly off so I need to reconsider the algorithm
-  function canvasToCart(canvas2: { x: number; y: number }) {
-    const xshift = vporigin.x / squareSize;
-    const yshift = vporigin.y / squareSize;
+	//TODO: Fix the y value conversion, seems to be slightly off so I need to reconsider the algorithm
+	function canvasToCart(canvas2: { x: number; y: number }) {
+		const xshift = vporigin.x / squareSize;
+		const yshift = vporigin.y / squareSize;
 
-    const leftBorderCart = xshift - scale / 2;
-    const topBorderCart = yshift + canvas.height / squareSize / 2;
+		const leftBorderCart = xshift - scale / 2;
+		const topBorderCart = yshift + canvas.height / squareSize / 2;
 
-    const xCart = leftBorderCart + canvas2.x / squareSize;
-    const yCart = topBorderCart - canvas2.y / squareSize;
+		const xCart = leftBorderCart + canvas2.x / squareSize;
+		const yCart = topBorderCart - canvas2.y / squareSize;
 
-    return {
-      x: xCart,
-      y: yCart,
-    };
-  }
+		return {
+			x: xCart,
+			y: yCart,
+		};
+	}
 
-  function getCenterCart() {
-    return [vporigin.x / scale, vporigin.y / squareSize / 2];
-  }
+	function getCenterCart() {
+		return [vporigin.x / scale, vporigin.y / squareSize / 2];
+	}
 
-  // function TlBl(canvasX: number, canvasY, width: number, height: number) {
-  function TlBl(width: number, height: number) {
-    let cartCent = getCenterCart();
+	// function TlBl(canvasX: number, canvasY, width: number, height: number) {
+	function TlBl(width: number, height: number) {
+		let cartCent = getCenterCart();
 
-    let yCart = height / 2 / scale;
-    let xCart = width / 2 / scale;
+		let yCart = height / 2 / scale;
+		let xCart = width / 2 / scale;
 
-    const tl = [cartCent.at(0) - xCart, cartCent.at(1) + yCart];
-    const bl = [cartCent.at(0) + xCart, cartCent.at(1) - yCart];
+		const tl = [cartCent.at(0) - xCart, cartCent.at(1) + yCart];
+		const bl = [cartCent.at(0) + xCart, cartCent.at(1) - yCart];
 
-    return [tl, bl];
-  }
+		return [tl, bl];
+	}
 
-  function drawGrid() {
-    if (!ctx) return;
+	function drawGrid() {
+		if (!ctx) return;
 
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
+		canvas.width = canvas.clientWidth;
+		canvas.height = canvas.clientHeight;
 
-    const origin = cartToCanvas({ x: 3, y: -1 });
-    // ctx.fillRect(origin.x, origin.y, 3, 3);
+		const origin = cartToCanvas({ x: 3, y: -1 });
+		// ctx.fillRect(origin.x, origin.y, 3, 3);
 
-    var spacing = canvas.width / scale;
-    var ycount = ceil(canvas.height / spacing);
-    // while (ycount >= 55) {
-    // 	spacing *= 2;
-    // 	ycount = ceil(canvas.height / spacing);
-    // }
+		var spacing = canvas.width / scale;
+		var ycount = ceil(canvas.height / spacing);
+		// while (ycount >= 55) {
+		// 	spacing *= 2;
+		// 	ycount = ceil(canvas.height / spacing);
+		// }
 
-    //TODO: Remove this scuffed as TlBl implentation in favor of the canvasToCart or cartToCanvas
-    tlbl = TlBl(canvas.width, canvas.height);
+		//TODO: Remove this scuffed as TlBl implentation in favor of the canvasToCart or cartToCanvas
+		tlbl = TlBl(canvas.width, canvas.height);
 
-    ctx.lineWidth = 0.2;
-    //INFO: draw vertical grid lines
-    for (let i = 0; i < scale + 1; i++) {
-      const offset = vporigin.x % squareSize;
-      const line = i * spacing - offset;
+		ctx.lineWidth = 0.2;
+		//INFO: draw vertical grid lines
+		for (let i = 0; i < scale + 1; i++) {
+			const offset = vporigin.x % squareSize;
+			const line = i * spacing - offset;
 
-      if (Math.round(canvasToCart({ x: line, y: 0 }).x) == 0) {
-        ctx.lineWidth = 1;
-      }
-      ctx.beginPath();
-      ctx.moveTo(line, 0);
-      ctx.lineTo(line, canvas.height);
-      ctx.stroke();
-      ctx.lineWidth = 0.2;
-    }
+			if (Math.round(canvasToCart({ x: line, y: 0 }).x) == 0) {
+				ctx.lineWidth = 1;
+			}
+			ctx.beginPath();
+			ctx.moveTo(line, 0);
+			ctx.lineTo(line, canvas.height);
+			ctx.stroke();
+			ctx.lineWidth = 0.2;
+		}
 
-    //WARNING: In the case of y, the line has to be drawn from the center outwards since you can't guarantee height
-    //INFO: draw horizontal grid lines
-    for (let i = 0; i < ceil(ycount / 2) + 1; i++) {
-      const centerY = canvas.height / 2;
-      const offset = vporigin.y % squareSize;
+		//WARNING: In the case of y, the line has to be drawn from the center outwards since you can't guarantee height
+		//INFO: draw horizontal grid lines
+		for (let i = 0; i < ceil(ycount / 2) + 1; i++) {
+			const centerY = canvas.height / 2;
+			const offset = vporigin.y % squareSize;
 
-      const line1 = centerY + i * spacing + offset;
-      const line2 = centerY - i * spacing + offset;
+			const line1 = centerY + i * spacing + offset;
+			const line2 = centerY - i * spacing + offset;
 
-      const line1Cart = Math.round(canvasToCart({ x: 0, y: line1 }).y);
-      const line2Cart = Math.round(canvasToCart({ x: 0, y: line2 }).y);
+			const line1Cart = Math.round(canvasToCart({ x: 0, y: line1 }).y);
+			const line2Cart = Math.round(canvasToCart({ x: 0, y: line2 }).y);
 
-      //NOTE: If one is 0, they both are
-      if (line1Cart == 0 || line2Cart == 0) {
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        line1Cart == 0 ? ctx.moveTo(0, line1) : ctx.moveTo(0, line2);
-        line1Cart == 0
-          ? ctx.lineTo(canvas.width, line1)
-          : ctx.lineTo(canvas.width, line2);
-        ctx.stroke();
-      } else {
-        ctx.lineWidth = 0.2;
-        ctx.beginPath();
-        ctx.moveTo(0, line1);
-        ctx.lineTo(canvas.width, line1);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.moveTo(0, line2);
-        ctx.lineTo(canvas.width, line2);
-        ctx.stroke();
-      }
-    }
+			//NOTE: If one is 0, they both are
+			if (line1Cart == 0 || line2Cart == 0) {
+				ctx.lineWidth = 2;
+				ctx.beginPath();
+				line1Cart == 0 ? ctx.moveTo(0, line1) : ctx.moveTo(0, line2);
+				line1Cart == 0
+					? ctx.lineTo(canvas.width, line1)
+					: ctx.lineTo(canvas.width, line2);
+				ctx.stroke();
+			} else {
+				ctx.lineWidth = 0.2;
+				ctx.beginPath();
+				ctx.moveTo(0, line1);
+				ctx.lineTo(canvas.width, line1);
+				ctx.stroke();
+				ctx.beginPath();
+				ctx.moveTo(0, line2);
+				ctx.lineTo(canvas.width, line2);
+				ctx.stroke();
+			}
+		}
 
-    renderAllChunks();
-  }
+		renderAllChunks();
+	}
 
-  $: if (latex_funcs) {
-    drawGrid();
-    renderAllChunks();
-  }
+	$: if (latex_funcs) {
+		drawGrid();
+		renderAllChunks();
+	}
 </script>
 
 <div id="graph" class="w-max bg-blue-100 flex-grow-[1]">
-  <canvas
-    bind:this={canvas}
-    id="canvas"
-    class="w-full h-full overflow-clip bg-bg-200"
-    tabindex="0"
-    on:keypress={(e) => {
-      if (e.key === "Z") {
-        scale = scale + 1;
-        latex_funcs = latex_funcs;
-      } else if (e.key === "z") {
-        scale = scale - 1;
-        latex_funcs = latex_funcs;
-      }
-    }}
-  >
-  </canvas>
+	<canvas
+		bind:this={canvas}
+		id="canvas"
+		class="w-full h-full overflow-clip bg-bg-200"
+		tabindex="0"
+		on:keypress={(e) => {
+			if (e.key === "Z") {
+				scale = scale + 1;
+				latex_funcs = latex_funcs;
+			} else if (e.key === "z") {
+				scale = scale - 1;
+				latex_funcs = latex_funcs;
+			}
+		}}
+	>
+	</canvas>
 </div>
--- src/lib/graphfunction.ts
+++ src/lib/graphfunction.ts
@@ -1,23 +1,21 @@
-import { v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
 export class GraphFunction {
-  latex: string;
-  color: string;
-  isVisible: boolean;
+	latex: string;
+	color: string;
+	isVisible: boolean;
 
-  readonly key: number;
+	readonly key: number;
 
-  constructor(graphFunction: Partial<GraphFunction>) {
-    this.latex = graphFunction.latex ?? ""; // Latex expression
-    this.color = graphFunction.color ?? ""; // Hex color code
-    this.isVisible = graphFunction.isVisible ?? true; // Visibility boolean
+	constructor(graphFunction?: Partial<GraphFunction>) {
+		this.latex = graphFunction?.latex ?? ""; // Latex expression
+		this.color = graphFunction?.color ?? ""; // Hex color code
+		this.isVisible = graphFunction?.isVisible ?? true; // Visibility boolean
 
-    if (graphFunction.key) {
-      throw new Error(
-        "Keys are computed internally and should not be passed into the SidebarElement constructor",
-      );
-    }
+		if (graphFunction?.key) {
+			throw new Error("Keys are computed internally and should not be passed into the SidebarElement constructor");
+		}
 
-    this.key = uuidv4(); // Unique generated ID
-  }
+		this.key = uuidv4(); // Unique generated ID
+	}
 }

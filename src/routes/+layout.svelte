f0f445752f55f715cc72a14e515e7f2c8c3e4984
masoniis <bott.m@comcast.net>
2024-04-16 12:05:59 -0600

    just adding vercel analytics and speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
 	import "../app.css";
 	import { MathQuillSetup } from "svelte-mathquill";
+
+	import { dev } from "$app/environment";
+	import { inject } from "@vercel/analytics";
+	import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+
+	injectSpeedInsights();
+	inject({ mode: dev ? "development" : "production" });
 </script>
 
 <MathQuillSetup />

3d5f9aa5222719f69fdcd511e143f6114f85924f
Simon Walker <simon@simonwalker.tech>
2024-10-25 12:45:47 -0600

    ReservedSymbol error type

--- src/lib/numerical/exprEvalError.ts
+++ src/lib/numerical/exprEvalError.ts
@@ -1,7 +1,7 @@
 export class ExprEvalError extends Error {
   constructor(message: string) {
     super(message);
-    this.name = "UnsupportedLatexError";
+    this.name = "ExpressionEvaluationError";
   }
 }
 
@@ -9,5 +9,19 @@ export class CircularDependencyError extends ExprEvalError {
   constructor() {
     super("Circular dependency detected");
     this.name = "CircularDependencyError";
+  }
+}
+
+export class ReservedSymbol extends ExprEvalError {
+  constructor(id: string) {
+    super("Reserved symbol detected: " + id);
+    this.name = "ReservedSymbolError";
+  }
+}
+
+export class Not2DGraphable extends ExprEvalError {
+  constructor() {
+    super("Not 2D graphable. Must be only a function of x or y, but not both");
+    this.name = "Not2DGraphableError";
   }
 }
